8fc14155923277e9dc777a4bc557e957772b24d9
Lizzy <lizabwth@gmail.com>
2014-12-27 06:43:59 -0800

    setup.py now places .pyc files in zip_includes
    rather than placing them in there manually

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
 import sys
+import os
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
                         "packages": ["os", "xml", "cgi", "hmac", "ctypes"], 
                         "excludes": ["tkinter"],
-                        "include_files": ["res/"]
+                        "include_files": ["res/"],
+                        "zip_includes":[]
                     }
+
+for fn in os.listdir("src/"):
+    if fn[-3::] != ".py":
+        if fn[-4::] != ".pyc":
+            fn += "/"
+        build_exe_options["zip_includes"].append(["src/"+fn,""])
+
+print build_exe_options["zip_includes"]
+
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
 base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
+# uncomment for no console window
+# if sys.platform == "win32":
+#     base = "Win32GUI"
 
 setup(  name = "Cutie",
         version = "0.1",
