ad2bfba35aa28cc11d9f59f02ba63204e141b2da
Lizzy <lizabwth@gmail.com>
2014-12-27 06:42:32 -0800

    resized join dialogue to accommodate for large addresses

--- src/joindialog.py
+++ src/joindialog.py
@@ -18,31 +18,31 @@ class Ui_JoinDialog(QtGui.QWidget):#class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName(_fromUtf8("Dialog"))
         Dialog.setWindowModality(QtCore.Qt.NonModal)
-        Dialog.resize(160, 100)
-        Dialog.setMinimumSize(QtCore.QSize(160, 100))
-        Dialog.setMaximumSize(QtCore.QSize(160, 100))
+        Dialog.resize(180, 100)
+        Dialog.setMinimumSize(QtCore.QSize(180, 100))
+        Dialog.setMaximumSize(QtCore.QSize(180, 100))
         Dialog.setFocusPolicy(QtCore.Qt.StrongFocus)
         Dialog.setModal(False)
 
         self.okCancle = QtGui.QDialogButtonBox(Dialog)
-        self.okCancle.setGeometry(QtCore.QRect(10, 70, 141, 21))
+        self.okCancle.setGeometry(QtCore.QRect(10, 70, 161, 21))
         self.okCancle.setOrientation(QtCore.Qt.Horizontal)
         self.okCancle.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
         self.okCancle.setObjectName(_fromUtf8("okCancle"))
 
         self.nameInput = QtGui.QLineEdit(Dialog)
-        self.nameInput.setGeometry(QtCore.QRect(10, 40, 141, 21))
+        self.nameInput.setGeometry(QtCore.QRect(10, 40, 161, 21))
         self.nameInput.setText(_fromUtf8(""))
         self.nameInput.setMaxLength(50)
         self.nameInput.setObjectName(_fromUtf8("nameInput"))
 
         self.addressInput = QtGui.QLineEdit(Dialog)
-        self.addressInput.setGeometry(QtCore.QRect(10, 10, 81, 21))
+        self.addressInput.setGeometry(QtCore.QRect(10, 10, 100, 21))
         self.addressInput.setText(_fromUtf8(""))
         self.addressInput.setObjectName(_fromUtf8("addressInput"))
 
         self.portInput = QtGui.QLineEdit(Dialog)
-        self.portInput.setGeometry(QtCore.QRect(100, 10, 51, 21))
+        self.portInput.setGeometry(QtCore.QRect(120, 10, 51, 21))
         self.portInput.setText(_fromUtf8(""))
         self.portInput.setObjectName(_fromUtf8("portInput"))
 
